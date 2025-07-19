30cd74603b1538ffcffbe27f0afe5a373acfe6d3
vishal-naga-rajan <vishalnagarajane@gmail.com>
2025-07-19 21:40:22 +0530

    Math Library

---
+++ math_lib/src/math_lib/Main.java
@@ -0,0 +1,19 @@
+package math_lib;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=20;
+		System.out.println("Max: "+Math.max(x, y));
+		System.out.println("Min: "+Math.min(x, y));
+		System.out.println("Square Root: "+Math.sqrt(64));
+		double d=-4.7766;
+		System.out.println("Absolute Value: "+Math.abs(d));
+		System.out.println("Random Value: "+(int)(Math.random()*100));
+		System.out.println(x>y);
+		System.out.println(x<y);
+	}
+
+}
