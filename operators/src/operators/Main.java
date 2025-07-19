67e465bf1c95f0a1262891f12291348c2e26e5e5
vishal-naga-rajan <vishalnagarajane@gmail.com>
2025-07-19 19:23:56 +0530

    operators

---
+++ operators/src/operators/Main.java
@@ -0,0 +1,20 @@
+package operators;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=6;
+		int y=2;
+		int z=10;
+		int sum=x+y+z;
+		double div=x/y;
+		int prod=x*y*z;
+		int rem=z%x;
+		System.out.println(sum);
+		System.out.println(div);
+		System.out.println(prod);
+		System.out.println(rem);
+	}
+
+}
