f854d6ac4e6f3e6dd75ce94034b5862df00426dc
vishal-naga-rajan <vishalnagarajane@gmail.com>
2025-07-19 15:47:17 +0530

    first commit

---
+++ Data_Types/src/data_types/Main.java
@@ -0,0 +1,22 @@
+package data_types;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Primitive Data_Types
+		int value=5;
+		double val=5.0;
+		boolean b=true;
+		char c='B';
+		String str="Hello World";
+		System.out.println(value);
+		System.out.println(val);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(str);
+		
+	}
+
+}
